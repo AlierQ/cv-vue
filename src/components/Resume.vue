612f7a09178fa0d8d3267c4ef4bf5c1aa07fb3d1
AlierQ <1029369938@qq.com>
2023-04-29 21:12:39 +0800

    feat：公共样式迁移

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -3,14 +3,40 @@
   <article>
     <!-- 个人资料 -->
     <Bio />
+    <!-- 项目经历 -->
+    <Projects />
   </article>
 </template>
 <script>
 import Bio from "./Bio.vue";
+import Projects from "./Projects.vue";
 export default {
   components: {
     Bio,
+    Projects,
   },
 };
 </script>
-<style scoped></style>
+<style>
+.icon {
+  vertical-align: middle;
+}
+
+.point-icon svg {
+  width: 10px;
+  height: 10px;
+  color: #254665;
+}
+
+.content-icon svg {
+  width: 28px;
+  height: 28px;
+  color: #254665;
+}
+
+.top-icon svg {
+  width: 18px;
+  height: 18px;
+  color: white;
+}
+</style>
